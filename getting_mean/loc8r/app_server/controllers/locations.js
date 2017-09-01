72464630a2c2f1a6e2240a81ffb9286c0feb8596
Vipin Patel <vipin.patel@neustar.biz>
2017-09-01 17:10:13 +0530

    additions to getting mean

---
+++ getting_mean/loc8r/app_server/controllers/locations.js
@@ -0,0 +1,14 @@
+module.exports.homelist = function(req, res, next)
+{
+  res.render('index', {title: 'Home'});
+};
+
+module.exports.locationInfo = function(req, res, next)
+{
+  res.render('index', {title: 'Location Info'});
+};
+
+module.exports.addReview = function(req, res, next)
+{
+  res.render('index', {title: 'New'});
+};
--- getting_mean/loc8r/app_server/routes/index.js
+++ getting_mean/loc8r/app_server/routes/index.js
@@ -1,7 +1,15 @@
 var express = require('express');
 var router = express.Router();
-var cntrlMain = require('../controllers/main.js');
-/* GET home page. */
-router.get('/', cntrlMain.index);
+var cntrlLocations = require('../controllers/locations');
+var cntrlOthers = require('../controllers/others');
+/* Location pages */
+router.get('/', cntrlLocations.homelist);
+router.get('/location', cntrlLocations.locationInfo);
+router.get('/location/reivew/new', cntrlLocations.addReview);
+
+/*About page*/
+
+router.get('/about', cntrlOthers.about);
+
 
 module.exports = router;
